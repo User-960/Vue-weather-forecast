45dac70f2ec91df41f00f43160a2dd603ee4137e
Ivan <ivanvolodin960@gmail.com>
2023-03-15 00:18:09 +0300

    refactor: change some styles

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
       <h1 class="weather__title">Weather Forecast</h1>
       <p class="weather__text">Check the weather in your city</p>
       <input class="weather__input" type="text" placeholder="Enter city">
-      <button class="btn primary">Get weather</button>
+      <button class="weather__btn btn primary">Get weather</button>
     </div>
   </div>
 </template>
@@ -25,5 +25,14 @@
 
 .weather__text {
   margin-top: 20px;
+}
+
+.weather__input {
+  margin-top: 30px;
+  display: inline-block;
+}
+
+.weather__btn {
+  margin-left: 20px;
 }
 </style>
