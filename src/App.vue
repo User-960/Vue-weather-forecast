351b8a9f48c1c7b827de1fc2ea3ce12283b5e48d
Ivan <ivanvolodin960@gmail.com>
2023-03-15 00:04:52 +0300

    feat: add html tags

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,29 @@
 <script></script>
 
-<template></template>
+<template>
+  <div class="container pt-5">
+    <div class="weather__inner card">
+      <h1 class="weather__title">Weather Forecast</h1>
+      <p class="weather__text">Check the weather in your city</p>
+      <input class="weather__input" type="text" placeholder="Enter city">
+      <button class="btn primary">Get weather</button>
+    </div>
+  </div>
+</template>
 
-<style scoped></style>
+<style scoped>
+.weather__inner {
+  height: 500px;
+  padding: 20px;
+  text-align: center;
+  color: #000;
+}
+
+.weather__title {
+  margin-top: 50px;
+}
+
+.weather__text {
+  margin-top: 20px;
+}
+</style>
